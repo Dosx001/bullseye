84911bc62eeaea9bae6cd242cd654ff4970ea7c5
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-06-24 15:24:51 -0700

    feat(tauri): create system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::{CustomMenuItem, Manager};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -8,8 +9,37 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let open = CustomMenuItem::new("open".to_string(), "Open");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let tray_menu = tauri::SystemTrayMenu::new()
+        .add_item(open)
+        .add_item(hide)
+        .add_native_item(tauri::SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    let system_tray = tauri::SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| {
+            if let tauri::SystemTrayEvent::MenuItemClick { id, .. } = event {
+                match id.as_str() {
+                    "open" => {
+                        let window = Manager::get_window(app, "main").unwrap();
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                    "hide" => {
+                        let window = Manager::get_window(app, "main").unwrap();
+                        window.hide().unwrap();
+                    }
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                }
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
